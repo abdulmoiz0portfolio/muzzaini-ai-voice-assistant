795a12c9b31f8ffb2c585a321ade1a117ecc20bc
abdulmoiz0portfolio <abdulmoizbaig50@gmail.com>
2025-08-23 19:18:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ javascript
 function sayHello() {
   alert("Hello!");
 }
+ - Confirm mobile browsers support it:
+     javascript
+     if ('speechSynthesis' in window) {
+       // safe to use
+     } else {
+       alert("Speech synthesis not supported on this browser.");
+     }
